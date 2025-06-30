09ecccf8fe479b3d504a79e2683d17709986d6ac
Genildo Lopes <jgmicroshs@gmail.com>
2025-06-30 01:28:09 -0300

    configuring app routes

--- routes/home.php
+++ routes/home.php
@@ -1,11 +1,11 @@
 <?php
 function loadContent()
 {
-  $page = isset($_GET['page']) ? basename($_GET['page']) : 'home'; // Evita ataques
+    $page = isset($_GET['page']) ? basename($_GET['page']) : 'index'; 
 
   $paths = [
-    __DIR__ . "/pages/{$page}.php",
-    __DIR__ . "/modules/{$page}.php"
+    __DIR__ . "/../controllers/" .$page . '.php',
+    __DIR__ . "/../modules/". $page . 'php',
   ];
 
   foreach ($paths as $path) {
@@ -15,34 +15,5 @@ function loadContent()
     }
   }
 
-  require_once(__DIR__ . '/404.php');
+  require_once(__DIR__ . './../views/home'); 
 }
-
-
-// function loadAbout()
-// {
-//   if (isset($_GET['page'])) {
-//     $page = $_GET['page'];
-//   } else {
-//     $page = 'home';
-//   }
-
-//   if (file_exists(__DIR__ . "/sobre/{$page}.php")) {
-//     require_once(__DIR__ . "/pages/{$page}.php");
-
-//     return;
-//   }
-
-//   require_once(__DIR__ . '/404.php');
-// }
-
-// function bootstrap()
-// {
-//   require_once(__DIR__ . '/templates/navbar.php');
-
-//   loadContent();
-
-//   require_once(__DIR__ . '/templates/footer.php');
-// }
-
-// bootstrap();
--- routes/index.php
+++ routes/index.php
@@ -1,48 +1,11 @@
-<?php
-function loadContent()
-{
-  $page = isset($_GET['page']) ? basename($_GET['page']) : 'home'; // Evita ataques
-
-  $paths = [
-    __DIR__ . "/pages/{$page}.php",
-    __DIR__ . "/modules/{$page}.php"
-  ];
-
-  foreach ($paths as $path) {
-    if (file_exists($path)) {
-      require_once($path);
-      return;
-    }
-  }
-
-  require_once(__DIR__ . '/404.php');
-}
-
-
-// function loadAbout()
-// {
-//   if (isset($_GET['page'])) {
-//     $page = $_GET['page'];
-//   } else {
-//     $page = 'home';
-//   }
-
-//   if (file_exists(__DIR__ . "/sobre/{$page}.php")) {
-//     require_once(__DIR__ . "/pages/{$page}.php");
-
-//     return;
-//   }
-
-//   require_once(__DIR__ . '/404.php');
-// }
-
-// function bootstrap()
-// {
-//   require_once(__DIR__ . '/templates/navbar.php');
-
-//   loadContent();
-
-//   require_once(__DIR__ . '/templates/footer.php');
-// }
-
-// bootstrap();
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+  <h1>TESTANDO ROTAS EM ROUTES</h1>
+</body>
+</html>
