09970f8efbc30e0e35abfa960d1e52e6ccaf3c81
Genildo Lopes <jgmicroshs@gmail.com>
2025-07-14 23:26:41 -0300

    modifield project files folders

---
+++ igreja/ministerio.php
@@ -0,0 +1,229 @@
+<div class="container-xxl py-5">
+  <div class="container">
+    <div class="row g-5">
+      <div class="col-lg-6 wow fadeInUp" data-wow-delay="0.1s">
+        <!-- <p class="fs-5 fw-medium text-primary">Get A Quote</p> -->
+        <h3 class="display-5 mb-4">Um Ministério forjado por Deus</h3>
+        <p>Where does it come from?
+          Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin
+          literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney
+          College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage,
+          and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem
+          Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and
+          Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the
+          Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section
+          1.10.32.</p>
+
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo quasi praesentium velit, repellendus
+          dolore omnis maiores ad asperiores ratione illum eveniet aut aliquid adipisci, quae expedita reprehenderit,
+          nobis assumenda suscipit.</p>
+
+        <p>Where does it come from?
+          Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin
+          literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney
+          College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage,
+          and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem
+          Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and
+          Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the
+          Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section
+          1.10.32.</p>
+
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo quasi praesentium velit, repellendus
+          dolore omnis maiores ad asperiores ratione illum eveniet aut aliquid adipisci, quae expedita reprehenderit,
+          nobis assumenda suscipit.</p>
+
+        <a class="d-inline-flex align-items-center rounded overflow-hidden border border-primary" href="">
+          <span class="btn-lg-square bg-primary" style="width: 55px; height: 55px;">
+            <i class="fa fa-phone-alt text-white"></i>
+          </span>
+          <span class="fs-5 fw-medium mx-4">+012 345 6789</span>
+        </a>
+      </div>
+
+      <div class="col-lg-6 wow fadeInUp" data-wow-delay="0.5s">
+        <h3 class="display-5 mb-4">Um Líder de Deus</h3>
+
+        <p>Where does it come from?
+          Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin
+          literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney
+          College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage,
+          and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem
+          Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and
+          Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the
+          Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section
+          1.10.32.</p>
+
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo quasi praesentium velit, repellendus
+          dolore omnis maiores ad asperiores ratione illum eveniet aut aliquid adipisci, quae expedita reprehenderit,
+          nobis assumenda suscipit.</p>
+
+        <p>Where does it come from?
+          Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin
+          literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney
+          College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage,
+          and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem
+          Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and
+          Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the
+          Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section
+          1.10.32.</p>
+
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo quasi praesentium velit, repellendus
+          dolore omnis maiores ad asperiores ratione illum eveniet aut aliquid adipisci, quae expedita reprehenderit,
+          nobis assumenda suscipit.</p>
+
+        <a class="d-inline-flex align-items-center rounded overflow-hidden border border-primary" href="">
+          <span class="btn-lg-square bg-primary" style="width: 55px; height: 55px;">
+            <i class="fa fa-phone-alt text-white"></i>
+          </span>
+          <span class="fs-5 fw-medium mx-4">+012 345 6789</span>
+        </a>
+      </div>
+    </div>
+  </div>
+</div>
+</div>
+
+<!-- Service Start -->
+<div class="container-xxl py-5">
+  <div class="container">
+    <div class="text-center mx-auto wow fadeInUp" data-wow-delay="0.1s" style="max-width: 500px;">
+      <!-- <p class="fs-5 fw-medium text-primary">Our Services</p> -->
+      <h4 class="display-12 mb-5">Digital Marketing Services that We Offer</h4>
+    </div>
+    <div class="row g-4">
+      <div class="col-lg-4 col-md-6 wow fadeInUp" data-wow-delay="0.1s">
+        <div class="service-item position-relative h-100">
+          <div class="service-text rounded p-5">
+            <div class="btn-square bg-light rounded-circle mx-auto mb-4" style="width: 64px; height: 64px;">
+              <img class="img-fluid" src="img/icon/icon-5.png" alt="Icon">
+            </div>
+            <h5 class="mb-3">Digital Marketing</h4>
+              <p class="mb-0">Erat ipsum justo amet duo et elitr dolor, est duo duo eos lorem sed diam
+                stet</p>
+          </div>
+          <div class="service-btn rounded-0 rounded-bottom">
+            <a class="text-primary fw-medium" href="">Read More<i class="bi bi-chevron-double-right ms-2"></i></a>
+          </div>
+        </div>
+      </div>
+      <div class="col-lg-4 col-md-6 wow fadeInUp" data-wow-delay="0.3s">
+        <div class="service-item position-relative h-100">
+          <div class="service-text rounded p-5">
+            <div class="btn-square bg-light rounded-circle mx-auto mb-4" style="width: 64px; height: 64px;">
+              <img class="img-fluid" src="img/icon/icon-6.png" alt="Icon">
+            </div>
+            <h5 class="mb-3">Internet Marketing</h4>
+              <p class="mb-0">Erat ipsum justo amet duo et elitr dolor, est duo duo eos lorem sed diam
+                stet</p>
+          </div>
+          <div class="service-btn rounded-0 rounded-bottom">
+            <a class="text-primary fw-medium" href="">Read More<i class="bi bi-chevron-double-right ms-2"></i></a>
+          </div>
+        </div>
+      </div>
+      <div class="col-lg-4 col-md-6 wow fadeInUp" data-wow-delay="0.5s">
+        <div class="service-item position-relative h-100">
+          <div class="service-text rounded p-5">
+            <div class="btn-square bg-light rounded-circle mx-auto mb-4" style="width: 64px; height: 64px;">
+              <img class="img-fluid" src="img/icon/icon-7.png" alt="Icon">
+            </div>
+            <h5 class="mb-3">Content Marketing</h4>
+              <p class="mb-0">Erat ipsum justo amet duo et elitr dolor, est duo duo eos lorem sed diam
+                stet</p>
+          </div>
+          <div class="service-btn rounded-0 rounded-bottom">
+            <a class="text-primary fw-medium" href="">Read More<i class="bi bi-chevron-double-right ms-2"></i></a>
+          </div>
+        </div>
+      </div>
+      <div class="col-lg-4 col-md-6 wow fadeInUp" data-wow-delay="0.1s">
+        <div class="service-item position-relative h-100">
+          <div class="service-text rounded p-5">
+            <div class="btn-square bg-light rounded-circle mx-auto mb-4" style="width: 64px; height: 64px;">
+              <img class="img-fluid" src="img/icon/icon-8.png" alt="Icon">
+            </div>
+            <h5 class="mb-3">Social Marketing</h4>
+              <p class="mb-0">Erat ipsum justo amet duo et elitr dolor, est duo duo eos lorem sed diam
+                stet</p>
+          </div>
+          <div class="service-btn rounded-0 rounded-bottom">
+            <a class="text-primary fw-medium" href="">Read More<i class="bi bi-chevron-double-right ms-2"></i></a>
+          </div>
+        </div>
+      </div>
+      <div class="col-lg-4 col-md-6 wow fadeInUp" data-wow-delay="0.3s">
+        <div class="service-item position-relative h-100">
+          <div class="service-text rounded p-5">
+            <div class="btn-square bg-light rounded-circle mx-auto mb-4" style="width: 64px; height: 64px;">
+              <img class="img-fluid" src="img/icon/icon-9.png" alt="Icon">
+            </div>
+            <h5 class="mb-3">B2B Marketing</h4>
+              <p class="mb-0">Erat ipsum justo amet duo et elitr dolor, est duo duo eos lorem sed diam
+                stet</p>
+          </div>
+          <div class="service-btn rounded-0 rounded-bottom">
+            <a class="text-primary fw-medium" href="">Read More<i class="bi bi-chevron-double-right ms-2"></i></a>
+          </div>
+        </div>
+      </div>
+      <div class="col-lg-4 col-md-6 wow fadeInUp" data-wow-delay="0.5s">
+        <div class="service-item position-relative h-100">
+          <div class="service-text rounded p-5">
+            <div class="btn-square bg-light rounded-circle mx-auto mb-4" style="width: 64px; height: 64px;">
+              <img class="img-fluid" src="img/icon/icon-10.png" alt="Icon">
+            </div>
+            <h5 class="mb-3">E-mail Marketing</h4>
+              <p class="mb-0">Erat ipsum justo amet duo et elitr dolor, est duo duo eos lorem sed diam
+                stet</p>
+          </div>
+          <div class="service-btn rounded-0 rounded-bottom">
+            <a class="text-primary fw-medium" href="">Read More<i class="bi bi-chevron-double-right ms-2"></i></a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+<!-- Service End -->
+
+
+<!-- Testimonial Start -->
+<div class="container-xxl pt-5">
+  <div class="container">
+    <div class="text-center text-md-start pb-5 pb-md-0 wow fadeInUp" data-wow-delay="0.1s" style="max-width: 500px;">
+      <p class="fs-5 fw-medium text-primary">Testimonial</p>
+      <h1 class="display-5 mb-5">What Clients Say About Our Services!</h1>
+    </div>
+    <div class="owl-carousel testimonial-carousel wow fadeInUp" data-wow-delay="0.1s">
+      <div class="testimonial-item rounded p-4 p-lg-5 mb-5">
+        <img class="mb-4" src="img/testimonial-1.jpg" alt="">
+        <p class="mb-4">Diam dolor diam ipsum sit. Aliqu diam amet diam et eos. Clita erat ipsum et lorem et
+          sit, sed stet lorem sit clita duo justo</p>
+        <h5>Client Name</h5>
+        <span class="text-primary">Profession</span>
+      </div>
+      <div class="testimonial-item rounded p-4 p-lg-5 mb-5">
+        <img class="mb-4" src="img/testimonial-2.jpg" alt="">
+        <p class="mb-4">Diam dolor diam ipsum sit. Aliqu diam amet diam et eos. Clita erat ipsum et lorem et
+          sit, sed stet lorem sit clita duo justo</p>
+        <h5>Client Name</h5>
+        <span class="text-primary">Profession</span>
+      </div>
+      <div class="testimonial-item rounded p-4 p-lg-5 mb-5">
+        <img class="mb-4" src="img/testimonial-3.jpg" alt="">
+        <p class="mb-4">Diam dolor diam ipsum sit. Aliqu diam amet diam et eos. Clita erat ipsum et lorem et
+          sit, sed stet lorem sit clita duo justo</p>
+        <h5>Client Name</h5>
+        <span class="text-primary">Profession</span>
+      </div>
+      <div class="testimonial-item rounded p-4 p-lg-5 mb-5">
+        <img class="mb-4" src="img/testimonial-4.jpg" alt="">
+        <p class="mb-4">Diam dolor diam ipsum sit. Aliqu diam amet diam et eos. Clita erat ipsum et lorem et
+          sit, sed stet lorem sit clita duo justo</p>
+        <h5>Client Name</h5>
+        <span class="text-primary">Profession</span>
+      </div>
+    </div>
+  </div>
+</div>
+<!-- Testimonial End -->
---
+++ mais/salvacao.php
@@ -0,0 +1,199 @@
+<?php require("navbar.php")    ?>
+<div class="container my-5">
+  <div class="row g-4">
+    <!-- Card 1 -->
+    <h1>Salvação em Jesus Cristo</h1>
+    <div class="col-lg-4 col-md-6">
+      <div class="card h-100">
+        <img src="img/carousel-2.jpg" alt="Image" class="card-img-top" alt="Card 1 Image">
+        <div class="card-body">
+          <h5 class="card-title">Antigo Testamento</h5>
+          <p class="card-text">
+            O Antigo Testamento é a primeira parte da Bíblia cristã, composta por 39 livros (no cânon protestante). Ele
+            foi escrito antes do nascimento de Jesus Cristo e contém a história, as leis, os profetas e os ensinamentos
+            do povo de Israel.
+          </p>
+          <a href="#" class="btn btn-primary">Learn More</a>
+        </div>
+      </div>
+    </div>
+    <!-- Card 2 -->
+    <div class="col-lg-4 col-md-6">
+      <div class="card h-100">
+        <img src="img/carousel-2.jpg" alt="Image" class="card-img-top" alt="Card 2 Image">
+        <div class="card-body">
+          <h5 class="card-title">Novo Testamento</h5>
+          <p class="card-text">
+            O Novo Testamento é a segunda parte da Bíblia cristã, composta por 27 livros que narram a vida,
+            ensinamentos, morte e ressurreição de Jesus Cristo, além do nascimento e expansão da Igreja primitiva.
+          </p>
+          <a href="#" class="btn btn-primary">Learn More</a>
+        </div>
+      </div>
+    </div>
+    <!-- Card 3 -->
+    <div class="col-lg-4 col-md-6">
+      <div class="card h-100">
+        <img src="img/carousel-2.jpg" alt="Image" class="card-img-top" alt="Card 3 Image">
+        <div class="card-body">
+          <h5 class="card-title">A graça de Deus</h5>
+          <p class="card-text">
+            A graça de Deus é amor e misericórdia que salva e transforma, independentemente de obras ou merecimentos
+            humanos pela fé em Jesus Cristo.
+          </p>
+          <a href="#" class="btn btn-primary">Learn More</a>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
+<div class="container-xxl py-5">
+  <div class="container">
+    <div class="row g-5">
+      <div class="col-lg-12 wow fadeInUp" data-wow-delay="0.1s">
+
+        <h1>A sua salvacão está em Jesus Cristo</h1>
+
+        <!-- <p>This part is inside a .container class.</p> -->
+
+        <p>Cremos na inspiração divina verbal e plenária da Bíblia sagrada, única regra infalível de fé e prática
+          para a
+          vida e o caráter cristão (II Tm 3:14-17);</p>
+
+        <p>Em um só Deus, eternamente subsistente em três pessoas distintas que, embora distintas, são iguais em
+          poder,
+          glória e majestade: o Pai, o Filho e o Espírito Santo; Criador do Universo, de todas as coisas que há nos
+          céus e
+          na terra, visíveis e invisíveis, e, de maneira especial, os seres humanos, por um ato sobrenatural e
+          imediato, e
+          não por um processo evolutivo (Dt.6:4; Mt.28:19; Mc.12:29; Gn1:1; 2:7; Hb.11:3 e Ap.4:11);
+        </p>
+
+        <p> No Senhor Jesus Cristo, o Filho Unigênito de Deus, plenamente Deus, plenamente Homem, na concepção e no
+          seu
+          nascimento virginal, em sua morte vicária e expiatória, em sua ressurreição corporal dentre os mortos e em
+          sua
+          ascensão vitoriosa aos céus como Salvador do mundo (Jo.3:16-18; Rm.1:3,4; Is.7:14; Mt.1:23; Hb.10:12;
+          Rm.8:34 e
+          At.1:9);</p>
+
+        <p>No Espírito Santo, a terceira pessoa da Santíssima Trindade, consubstancial com o Pai e o Filho, Senhor e
+          Vivificador; que convence o mundo do pecado, da justiça e do juízo; que regenera o pecador; que falou por
+          meio
+          dos profetas e continua guiando o seu povo (II Cor.13:13; II Cor.3:6,17; Rm.8:2; Jo.16:11; Tt.3:5; II Pe
+          1:21 e
+          Jo.16:13);</p>
+
+        <p>Na pecaminosidade do homem, que o destituiu da glória de Deus e que somente o arrependimento e a fé na
+          obra
+          expiatória e redentora de Jesus Cristo podem restaurá-lo a Deus (Rm.3:23; At.3:19);</p>
+
+        <p>Na necessidade absoluta do novo nascimento pela graça de Deus mediante a fé em Jesus Cristo e pelo poder
+          atuante
+          do Espírito santo para tornar o homem aceito no Reino dos Céus (Jo.3:3-8; Ef.2:8,9);
+
+        <p>No perdão dos pecados, na salvação plena e na justificação pela fé no sacrifício efetuado por Jesus
+          Cristo em
+          nosso favor (At.10:43; Rm.10:13; 3:24-26; Hb.7:25; 5:9);</p>
+
+        <p> Na Igreja, que é o corpo de Cristo, coluna e firmeza da verdade, una, santa e universal assembleia dos
+          fiéis
+          remidos de todas as eras e de todos os lugares, chamados do mundo pelo Espírito Santo para seguir a
+          Cristo e
+          adorar a Deus (I Cor.12:27; Jo.4:23; I Tim.3:15; Hb.12:23 e Ap.22:17);</p>
+
+        <p>No batismo bíblico efetuado por imersão em águas, uma só vez, em nome do Pai, e do Filho, e do Espírito
+          Santo,
+          conforme determinou o Senhor Jesus Cristo (Mt.28:19; Rm.6:1-6; Cl. 2:12);</p>
+
+        <p>Na necessidade e na possibilidade de termos vida santa e irrepreensível por obra do Espírito Santo, que
+          nos
+          capacita a viver como fiéis testemunhas de Jesus Cristo (Hb.9:14; I Pe 1:15);</p>
+
+        <p>No batismo no Espírito Santo, conforme as Escrituras, que nos é dado por Jesus Cristo, demonstrado pela
+          evidência física do falar em outras línguas, conforme a sua vontade (At.1:5; 2:4; 10:44-46; 19:1-7);</p>
+
+        <p> Na atualidade dos dons espirituais distribuídos pelo Espírito Santo à Igreja para sua edificação,
+          conforme
+          sua
+          soberana vontade para o que for útil (I Co.12:1-12);</p>
+
+        <p>Na segunda vinda de Cristo, em duas fases distintas: a primeira – invisível ao mundo, para arrebatar a
+          sua
+          Igreja antes da Grande Tribulação; a segunda – visível e corporal, com a sua Igreja glorificada, para
+          reinar
+          sobre o mundo durante mil anos (I Ts.4:16,17; I Co.15:51-54; Ap.20:4; Zc.14:5 e Jd.1:14);</p>
+
+        <p>No comparecimento ante o Tribunal de Cristo de todos os cristãos arrebatados, para receberem a recompensa
+          pelos
+          seus feitos em favor da causa de Cristo na Terra (II Co.5:10);</p>
+
+        <p> No Juízo Final, onde comparecerão todos os ímpios; desde a Criação até o fim do Milênio, os que morrerem
+          durante
+          o período milenial e os que, ao final desta época, estiverem vivos. E na Eternidade de tristeza e
+          tormento
+          para
+          os infiéis e vida eterna de gozo e felicidade para os fiéis de todos os tempos (Mt.25:46; Is.65:20;
+          Ap.20:11-15;
+          21:1-4);</p>
+
+        <p>Cremos, também, que o casamento foi instituído por Deus e retificado por nosso Senhor Jesus Cristo como
+          união
+          entre um homem e uma mulher, nascidos macho e fêmea, respectivamente, em conformidade com o definido
+          pelo
+          sexo
+          de criação geneticamente determinado (Gn.2:18; Jo.2:1,2; Gn.2:24; 1:27).</p>
+
+      </div>
+
+      <!-- Testimonial Start -->
+      <div class="container-xxl pt-5">
+        <div class="container">
+          <div class="text-center text-md-start pb-5 pb-md-0 wow fadeInUp" data-wow-delay="0.1s"
+            style="max-width: 500px;">
+            <p class="fs-5 fw-medium text-primary">Testimonial</p>
+            <h1 class="display-5 mb-5">What Clients Say About Our Services!</h1>
+          </div>
+          <div class="owl-carousel testimonial-carousel wow fadeInUp" data-wow-delay="0.1s">
+            <div class="testimonial-item rounded p-4 p-lg-5 mb-5">
+              <img class="mb-4" src="img/testimonial-1.jpg" alt="">
+              <p class="mb-4">Diam dolor diam ipsum sit. Aliqu diam amet diam et eos. Clita erat ipsum et lorem et
+                sit, sed stet lorem sit clita duo justo</p>
+              <h5>Client Name</h5>
+              <span class="text-primary">Profession</span>
+            </div>
+            <div class="testimonial-item rounded p-4 p-lg-5 mb-5">
+              <img class="mb-4" src="img/testimonial-2.jpg" alt="">
+              <p class="mb-4">Diam dolor diam ipsum sit. Aliqu diam amet diam et eos. Clita erat ipsum et lorem et
+                sit, sed stet lorem sit clita duo justo</p>
+              <h5>Client Name</h5>
+              <span class="text-primary">Profession</span>
+            </div>
+            <div class="testimonial-item rounded p-4 p-lg-5 mb-5">
+              <img class="mb-4" src="img/testimonial-3.jpg" alt="">
+              <p class="mb-4">Diam dolor diam ipsum sit. Aliqu diam amet diam et eos. Clita erat ipsum et lorem et
+                sit, sed stet lorem sit clita duo justo</p>
+              <h5>Client Name</h5>
+              <span class="text-primary">Profession</span>
+            </div>
+            <div class="testimonial-item rounded p-4 p-lg-5 mb-5">
+              <img class="mb-4" src="img/testimonial-4.jpg" alt="">
+              <p class="mb-4">Diam dolor diam ipsum sit. Aliqu diam amet diam et eos. Clita erat ipsum et lorem et
+                sit, sed stet lorem sit clita duo justo</p>
+              <h5>Client Name</h5>
+              <span class="text-primary">Profession</span>
+            </div>
+          </div>
+        </div>
+      </div>
+
+    </div>
+  </div>
+</div>
+
+
+
+<!-- Testimonial End -->
+<!-- Footer End -->
+<?php require("footer.php")    ?>
